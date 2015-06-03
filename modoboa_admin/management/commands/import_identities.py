494ee2797cd4184d24e65645145a479fa51dfa41
Stéphane Klein <contact@stephane-klein.info>
2015-06-03 18:15:13 +0200

    Append import csv cli commands https://github.com/modoboa/modoboa-admin/issues/15

---
+++ modoboa_admin/management/commands/import_identities.py
@@ -0,0 +1,74 @@
+import sys
+import os.path
+import csv
+from optparse import make_option
+
+from modoboa.core.models import User
+from django.core.management.base import BaseCommand
+
+
+from modoboa.core import load_core_settings
+from modoboa.core.management.commands import CloseConnectionMixin
+from modoboa.lib import events
+from modoboa.lib.exceptions import Conflict
+from modoboa_admin.views.import_ import (  # NOQA
+    import_account, import_alias, import_forward, import_dlist
+)
+
+from modoboa.core.extensions import exts_pool
+
+
+class Command(BaseCommand, CloseConnectionMixin):
+    args = 'csvfile'
+    help = 'Import identities csv file'
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--sepchar', action='store_true', dest='sepchar', default=';'
+        ),
+        make_option(
+            '--continue-if-exists', action='store_true',
+            dest='continue_if_exists', default=True
+        ),
+        make_option(
+            '--crypt-password', action='store_true', dest='crypt_password',
+            default=False
+        )
+    )
+
+    def handle(self, *args, **kwargs):
+        superadmin = User.objects.filter(is_superuser=True).first()
+        exts_pool.load_all()
+        load_core_settings()
+
+        for filename in args:
+            if not os.path.exists(filename):
+                print('File not found')
+                sys.exit(1)
+
+            with open(filename, 'r') as f:
+                reader = csv.reader(f, delimiter=';')
+                for row in reader:
+                    if not row:
+                        continue
+
+                    try:
+                        fct = globals()["import_%s" % row[0].strip()]
+                    except KeyError:
+                        fct = events.raiseQueryEvent(
+                            'ImportObject', row[0].strip())
+                        if not fct:
+                            continue
+                        fct = fct[0]
+
+                    print(row)
+                    try:
+                        fct(superadmin, row, kwargs)
+                    except Conflict:
+                        if kwargs['continue_if_exists']:
+                            continue
+
+                        raise Conflict(
+                            "Object already exists: %s"
+                            % kwargs['sepchar'].join(row[:2])
+                        )
